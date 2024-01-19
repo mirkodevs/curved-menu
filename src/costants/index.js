33b07565464ba93afa78c4ba20236f88801707bb
mirkodevs <devmirko06@gmail.com>
2024-01-19 16:45:25 +0100

    initial commit

---
+++ src/costants/index.js
@@ -0,0 +1,19 @@
+ const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Portfolio",
+    href: "/portfolio",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+]
+export  default navItems
